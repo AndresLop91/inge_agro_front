293a335186378afbf82255101dbf81e56cc3d879
AndresLop91 <afelipe-18@hotmail.com>
2023-09-06 09:33:36 -0500

    implementacion carousel

---
+++ src/app/carrusel/carrusel.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CarruselComponent } from './carrusel.component';
+
+describe('CarruselComponent', () => {
+  let component: CarruselComponent;
+  let fixture: ComponentFixture<CarruselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CarruselComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarruselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  // Aquí puedes agregar más pruebas unitarias para tu componente si es necesario.
+
+});
---
+++ src/app/carrusel/carrusel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.scss']
+})
+export class CarruselComponent {
+  // Aquí puedes agregar la lógica del componente, como cargar las imágenes y datos del carrusel.
+}
